3de58e31bc5562603bcfdeeaf89f7fbaee4b4a04
marta.pancaldi <marta.pancaldi@resourcify.de>
2023-09-09 11:53:54 +0200

    Parse Idealista page - wip fix 403 errors

--- idealista_scraper.py
+++ idealista_scraper.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
 
-headers = ["Title", "Link", "Type", "State", "Asking", "Notes", "Where",
+headers = ["Title", "Link", "Type", "Asking", "Notes", "Where",
            "Size (garden)", "Rooms", "Rating", "Plan", "Agent", "Status"]
 
-with open("data/immobiliare_urls.txt", "r") as url_file:
+with open("data/idealista_urls.txt", "r") as url_file:
     urls = [line.strip() for line in url_file if line.strip()]
 
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    output_filename = f"output/immobiliare_{timestamp}.csv"
+    output_filename = f"output/idealista_{timestamp}.csv"
 
     with open(output_filename, mode="w", newline="") as csv_file:
         writer = csv.writer(csv_file)
@@ -23,33 +23,25 @@ with open("data/immobiliare_urls.txt", "r") as url_file:
                 soup = BeautifulSoup(response.text, 'html.parser')
 
                 # Title
-                title = soup.find("h1", class_="in-titleBlock__title").text.strip()
+                title = soup.find("span", class_="main-info__title-main").text.strip()
 
                 # Price
-                price = soup.find("li", class_="in-detail__mainFeaturesPrice").text.strip()
-                price = price.replace("€", "").replace(".", "").strip()
+                span_element = soup.find("span", class_="info-data-price").text.strip()
+                price = span_element.find("span", class_="txt-bold").text.strip()
 
-                # Rooms
-                li_element = soup.find("li", {"aria-label": "locali"})
-                div_element = li_element.find("div", class_="in-feat__data")
-                rooms = div_element.text.strip()
-
-                # Size
-                li_element = soup.find("li", {"aria-label": "superficie"})
-                div_element = li_element.find("div", class_="in-feat__data")
-                size = div_element.text.strip()
-
-                # Description
-                description = soup.find("p", class_="in-description__title").text.strip()
+                # Rooms & Size
+                div_element = soup.find("div", class_="info-features")
+                spans = div_element.find_all("span")
+                size = spans[0].text.strip()
+                rooms = spans[1].text.replace("locali", "").strip()
 
                 # Type
-                dt_element = soup.find("dt", text="tipologia")
-                dd_element = dt_element.find_next_sibling("dd")
-                property_type = dd_element.text.strip()
+                div_element = soup.find("div", class_="details-property_features")
+                li_elements = div_element.find_all("li")
+                property_type = li_elements[0].text.strip()
 
                 # Agent
-                div_element = soup.find("div", class_="in-referent")
-                a_element = div_element.find("a")
+                a_element = soup.find("a", class_="about-advertiser-name")
                 agent = a_element.text.strip()
 
                 # Print the extracted data
@@ -58,12 +50,11 @@ with open("data/immobiliare_urls.txt", "r") as url_file:
                 print("Price:", price)
                 print("Size:", size)
                 print("Rooms:", rooms)
-                print("Description:", description)
                 print("Type:", property_type)
                 print("Agent:", agent)
                 print("-------")
 
-                writer.writerow([title, url, property_type, description, price, "", "", size, rooms, "", "", agent, ""])
+                writer.writerow([title, url, property_type, price, "", "", size, rooms, "", "", agent, ""])
 
             else:
                 print("Failed to retrieve the webpage. Status code:", response.status_code)
